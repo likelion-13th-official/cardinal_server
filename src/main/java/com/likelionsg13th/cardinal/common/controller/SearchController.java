0a84f602894d29ce5c5ced26f6affce8870e0fd5
naaadang <naaadang@naver.com>
2025-08-22 08:39:00 +0900

    feat: 전체 검색 결과 조회

---
+++ src/main/java/com/likelionsg13th/cardinal/common/controller/SearchController.java
@@ -0,0 +1,28 @@
+package com.likelionsg13th.cardinal.common.controller;
+
+import com.likelionsg13th.cardinal.common.dto.resonseDto.ApiResponse;
+import com.likelionsg13th.cardinal.common.dto.resonseDto.search.SearchResponse;
+import com.likelionsg13th.cardinal.common.service.SearchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/search")
+public class SearchController {
+    private final SearchService searchService;
+
+    /* 전체 검색 결과 초기화면*/
+    @GetMapping()
+    public ResponseEntity<ApiResponse> searchAll(
+            @RequestParam("query") String query
+    ){
+        SearchResponse response=searchService.searchAll(query);
+        return ResponseEntity.ok(new ApiResponse(true,200,"검색결과 초기화면 조회 성공",response));
+    }
+
+}
