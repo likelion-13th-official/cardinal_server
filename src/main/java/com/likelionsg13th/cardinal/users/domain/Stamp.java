ea22ea11eeafb95b626322b608706334b80b66fa
naaadang <naaadang@naver.com>
2025-08-29 08:25:22 +0900

    feat: Stamp 도메인 수정

--- src/main/java/com/likelionsg13th/cardinal/users/domain/Stamp.java
+++ src/main/java/com/likelionsg13th/cardinal/users/domain/Stamp.java
@@ -1,4 +1,24 @@
 package com.likelionsg13th.cardinal.users.domain;
 
+import jakarta.persistence.*;
+import org.apache.catalina.User;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class Stamp {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id")
+    private Users user;
+
+    @Enumerated(EnumType.STRING)
+    private ActivityType activityType;
+
+    private LocalDateTime timestamp;
+
 }
