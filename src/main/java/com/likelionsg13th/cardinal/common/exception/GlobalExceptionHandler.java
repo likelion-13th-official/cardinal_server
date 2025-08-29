bae0eccc60509a3c4797b3cac991108ff5e12395
naaadang <naaadang@naver.com>
2025-08-29 09:28:33 +0900

    feat: 스탬프 생성 기능 구현

--- src/main/java/com/likelionsg13th/cardinal/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/likelionsg13th/cardinal/common/exception/GlobalExceptionHandler.java
@@ -10,16 +10,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Arrays;
+import java.util.List;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-
-
-
 
     ///커스텀예외 (비즈니스)
     @ExceptionHandler(BusinessException.class)
@@ -36,6 +39,15 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MissingServletRequestParameterException.class)
     public ResponseEntity<ApiResponse> handleMissingParameter(MissingServletRequestParameterException ex) {
         String message = ex.getParameterName() + " parameter is missing.";
+        ApiResponse response = new ApiResponse(false,HttpStatus.BAD_REQUEST.value(), message);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    /*Request Body Missing*/
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+
+        String message = Arrays.toString(ex.getDetailMessageArguments()) + " argument is missing.";
         ApiResponse response = new ApiResponse(false,HttpStatus.BAD_REQUEST.value(), message);
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
