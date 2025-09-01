0e4c08e52dae39e27c9121e5a7cc26b0218c4e48
yuyi5187 <kimyy200063@gmail.com>
2025-09-01 22:44:43 +0900

    [feat] scrap 적용중

--- src/main/java/com/likelionsg13th/cardinal/performance/dto/PerformanceResponse.java
+++ src/main/java/com/likelionsg13th/cardinal/performance/dto/PerformanceResponse.java
@@ -23,13 +23,14 @@ public class PerformanceResponse {
 
     private boolean isScrapped;
 
-    public static PerformanceResponse from(Performance performance) {
+    public static PerformanceResponse from(Performance performance, boolean scrapped) {
         return PerformanceResponse.builder()
                 .id(performance.getId())
                 .name(performance.getName())
                 .category(performance.getCategory() == null ? null: performance.getCategory().toKorean())
                 .description(performance.getDescription())
                 //TO-DO 북마크
+                .isScrapped(scrapped)
                 .thumbnail(performance.getThumbnailUrl())
                 .operatingDays(
                         performance.getOperatingDays().stream()
