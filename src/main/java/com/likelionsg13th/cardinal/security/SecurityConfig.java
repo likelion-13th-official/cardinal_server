e05dfef4536b62a5d6f6f865c90cf8cf7d117ce4
naaadang <126650849+naaadang@users.noreply.github.com>
2025-09-03 02:18:57 +0900

    Update SecurityConfig.java

--- src/main/java/com/likelionsg13th/cardinal/security/SecurityConfig.java
+++ src/main/java/com/likelionsg13th/cardinal/security/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/auth/**",
-                                "/booths/**",
+                                "/booths/**","/booths",
                                 "/events/**", "/events",
                                 "/goods/**", "/goods",
                                 "/performances/** ", "/performances",
