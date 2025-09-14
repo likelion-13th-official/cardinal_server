7fd21d6c3be4c27e5fd20283f5ff59062fa6562a
yeeun <drrrkak@gmail.com>
2025-09-14 17:13:39 +0900

    fix: error scrap delete

--- src/main/java/com/likelionsg13th/cardinal/users/controller/ScrapController.java
+++ src/main/java/com/likelionsg13th/cardinal/users/controller/ScrapController.java
@@ -44,7 +44,7 @@ public class ScrapController {
 
     @DeleteMapping("/{scrapId}")
     public ResponseEntity<ApiResponse> DeleteScrapsById(@AuthenticationPrincipal @Valid UserDetails principal
-                                                        ,@PathVariable @Valid @NotBlank Long scrapId) {
+                                                        ,@PathVariable  Long scrapId) {
         UserDto user = userService.getMeBySubject(principal.getUsername());
         scrapService.deleteScrapByScrapId(user, scrapId);
         return ResponseEntity.ok(new ApiResponse(true, 200, "스크랩 삭제 성공",null));
