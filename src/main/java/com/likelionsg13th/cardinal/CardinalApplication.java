a5f69eb526d2867f9fdc831baff5d3b2084b9aed
yuyi5187 <kimyy200063@gmail.com>
2025-09-18 21:24:15 +0900

    [fix] timezone setting

--- src/main/java/com/likelionsg13th/cardinal/CardinalApplication.java
+++ src/main/java/com/likelionsg13th/cardinal/CardinalApplication.java
@@ -1,14 +1,22 @@
 package com.likelionsg13th.cardinal;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @EnableAsync @EnableScheduling
 @SpringBootApplication
 public class CardinalApplication {
+
+	@PostConstruct
+	public void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CardinalApplication.class, args);
